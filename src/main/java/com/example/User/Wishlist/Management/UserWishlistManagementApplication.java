42661a4d96f91382e119140011b5c92467967e97
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2024-02-13 17:41:01 +0530

    Start commit

---
+++ src/main/java/com/example/User/Wishlist/Management/UserWishlistManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.User.Wishlist.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWishlistManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWishlistManagementApplication.class, args);
+	}
+
+}
